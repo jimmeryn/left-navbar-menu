e6888190f15d6833d7b9dd40aa03189cd59d3fc4
jimmeryn <jimmeryn@gmail.com>
2019-09-11 15:21:03 +0200

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import "../styles/style.css";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">Left Menu App</header>
+        <div className="Container"></div>
+      </div>
+    );
+  }
+}
